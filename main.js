fcb5b73c5b5956d4ec70e4edd51ebdafda66c1d7
Nathan Hill <nathan.hill@mfg.at>
2025-06-13 11:29:47 +0200

    logik einbauen

--- main.js
+++ main.js
@@ -1,12 +1,48 @@
+let firstInput = '';
+let firstNumber = '';
+let currentOperator = '';
+let secondNumber = '';
+let result = 0;
+
 const buttons = document.querySelectorAll('.card-button');
 
 buttons.forEach(button => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (e) => {
         const input = document.querySelector('.card-input');
+
         if (button.dataset.numeric) {
             input.value += button.dataset.numeric;
+            firstInput += button.dataset.numeric;
+        } else if (button.dataset.operator === 'ac') {
+            input.value = '';
+            firstNumber = '';
+            secondNumber = '';
+            currentOperator = '';
+            result = 0;
+        } else if (button.dataset.operator === '=') {
+            secondNumber = input.value;
+            switch (currentOperator) {
+                case '+':
+                    result = Number(firstNumber) + Number(secondNumber);
+                    break;
+                case '-':
+                    result = Number(firstNumber) - Number(secondNumber);
+                    break;
+                case '*':
+                    result = Number(firstNumber) * Number(secondNumber);
+                    break;
+                case '/':
+                    result = Number(firstNumber) / Number(secondNumber);
+                    break;
+            }
+            input.value = result;
+            firstNumber = '';
+            secondNumber = '';
+            currentOperator = '';
+        } else {
+            firstNumber = input.value;
+            currentOperator = button.dataset.operator;
+            input.value = '';
         }
-        console.log(input.value);
-        
     });
 });
